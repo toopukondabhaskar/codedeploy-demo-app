65f8c458a899c8ff94e16cc9a70a9021b9324f9c
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-02-19 10:03:55 +0530

    scripts file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,9 @@
 echo "printing"
 CURRENT_BUILD_NO=$(expr $BUILD_NO - 1)
 echo $CURRENT_BUILD_NO
-cd /home/ubuntu/circleci-poc
+cd /home/ubuntu/codedeploy-demo-app
 rm -rf *
-wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
-cd /home/ubuntu/circleci-poc/usr/src/app
-sudo docker system prune -af
-sudo docker rmi -f madhantry/mdn-images:latest
-sudo docker pull madhantry/mdn-images:latest && sudo docker-compose up -d
+wget https://$CURRENT_BUILD_NO-169070966-gh.circle-artifacts.com/0/home/ubuntu && unzip artifact.zip
+
 
 
